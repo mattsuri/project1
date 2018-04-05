cd8c297cd38db7fab17e8f6e0db16a351c2d1f46
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-04-05 08:33:52 -0400

    Updated from Brython Server: 4/5/2018 8:33:48 AM

--- whackAMole.py
+++ whackAMole.py
@@ -10,10 +10,17 @@ green = Color(0x00FF00, 1)
 blue = Color(0x0000FF, 1)
 black = Color(0x000000, 1) 
 
+def mouseClick(event):
+    print(event.x)
+    print(event.y)
+    if event.x < 200 and event.y < 200:
+        print("1")
+        
 
 
 
     
+
 
 
 
@@ -21,10 +28,13 @@ if __name__ == "__main__":
     blackOutline = LineStyle(1, black)
     
     
-    redCircle = CircleAsset(50, blackOutline, red) #radius, outline, fill
-    for i in range (0,5):
-        height = 50 + i*150
-        for i in range (0,5):
-            Sprite(redCircle, (20 + i*150, height ))
+    redCircle = CircleAsset(100, blackOutline, red) #radius, outline, fill
+    for i in range (0,3):
+        height = 50 + i*250
+        for i in range (0,3):
+            Sprite(redCircle, (20 + i*250, height ))
+            
+            
     
+    App().listenMouseEvent("click", mouseClick)
     App().run() 
