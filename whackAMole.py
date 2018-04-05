20cf1187d7ee6e14a84861ab1866343f7b7e712c
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-04-05 08:19:24 -0400

    Updated from Brython Server: 4/5/2018 8:19:19 AM

--- whackAMole.py
+++ whackAMole.py
@@ -2,4 +2,29 @@
 #4/4/18
 #whackAMole.py - whack a mole 
 
-def circles 
+
+from ggame import *
+
+red = Color(0xFF0000, 1) #this is the color red
+green = Color(0x00FF00, 1)
+blue = Color(0x0000FF, 1)
+black = Color(0x000000, 1) 
+
+
+
+
+    
+
+
+
+if __name__ == "__main__":
+    blackOutline = LineStyle(1, black)
+    
+    
+    redCircle = CircleAsset(50, blackOutline, red) #radius, outline, fill
+    for i in range (0,5):
+        height = 50 + i*150
+        for i in range (0,5):
+            Sprite(redCircle, (20 + i*150, height ))
+    
+    App().run() 
